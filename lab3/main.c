9d43a05f2a16e2c6781def65f1206240116fe38a
Nikitosik030 <nikitaanisimov321@gmail.com>
2025-02-18 18:34:42 +1000

    Add files via upload

---
+++ lab3/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    char stroka[80];
+    printf("vvedite stroky: ");
+    scanf("%s", stroka);
+    char *p_str = stroka;
+    while(*p_str){
+    if(*p_str == 'a'){
+    *p_str = 'A';
+    }
+    if(*p_str == 'b'){
+    *p_str = 'B';
+    }
+    *p_str ++;
+    }
+    printf("%s", stroka);
+    return 0;
+}
