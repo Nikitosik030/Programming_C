146f2363d149470ec7cdc738c959113d1159644b
Nikitosik030 <nikitaanisimov321@gmail.com>
2025-03-15 12:35:05 +1000

    Update triangl.c

--- lab4/triangl.c
+++ lab4/triangl.c
@@ -6,6 +6,6 @@ double calculate_perimeter(double a, double b, double c) {
 }
 
 double calculate_area(double a, double b, double c) {
-    double s = (a + b + c) / 2; // полупериметр
+    double s = (a + b + c) / 2; 
     return sqrt(s * (s - a) * (s - b) * (s - c));
 }
