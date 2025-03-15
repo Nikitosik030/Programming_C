a93f107e4d5a186bd0abfad7a26f9fbeb7807f52
Nikitosik030 <nikitaanisimov321@gmail.com>
2025-03-15 13:15:00 +1000

    Update triangl.h

--- lab4/triangl.h
+++ lab4/triangl.h
@@ -3,5 +3,5 @@
 
 double calculate_perimeter(double a, double b, double c);
 double calculate_area(double a, double b, double c);
-
-#endif // TRIANGLE_H
+int is_valid_triangle(double a, double b, double c);
+#endif 
