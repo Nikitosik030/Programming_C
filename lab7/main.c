8e5ed605a811ecfd3c446ff33d3d66256e60ee40
Nikitosik030 <nikitaanisimov321@gmail.com>
2025-04-11 22:40:45 +1000

    Update main.c

--- lab7/main.c
+++ lab7/main.c
@@ -14,7 +14,6 @@ void sortHumansByYear(human *array, int size) {
     for (int i = 0; i < size - 1; i++) {
         for (int j = 0; j < size - i - 1; j++) {
             if (array[j].birthYear > array[j + 1].birthYear) {
-                // Swap structures
                 human temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
