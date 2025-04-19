1ed5af62e288f10a6095d6914dc9180f62c7a555
Nikitosik030 <nikitaanisimov321@gmail.com>
2025-04-19 12:33:43 +1000

    Update main.c

--- lab10/main.c
+++ lab10/main.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define MAX(arr, n) ({ \
+    int _max = (arr)[0]; \
+    for (int i = 1; i < (n); i++) { \
+        if ((arr)[i] > _max) { \
+            _max = (arr)[i]; \
+        } \
+    } \
+    _max; })
 
 int main() {
-    int n;
+    int count;
+    printf("Vvedite kolichestvo chisel: ");
+    scanf("%d", &count);
 
-    printf("¬ведите число: ");
-    scanf("%d", &n);
-
-    printf(" вадраты чисел от 1 до %d:\n", n);
-    for (int i = 1; i <= n; i++) {
-        printf("%d в квадрате равно %d\n", i, i * i);
+    int *numbers = (int*)malloc(count * sizeof(int));
+    printf("Vvedite %d chisel cherez probel: ", count);
+    for (int i = 0; i < count; i++) {
+        scanf("%d", &numbers[i]);
     }
 
+    int max_num = MAX(numbers, count);
+    printf("Maksimalnoe chislo: %d\n", max_num);
+
+    free(numbers);
     return 0;
 }
